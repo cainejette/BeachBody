14d7b7e7aaf96f85c62dd99f550cf46919b6d2d7
cjette <jette@alum.mit.edu>
2014-11-23 15:48:33 -0800

    Added AutoFocusTextBox to autofocus on load

---
+++ BeachBody Workout Tracker/Common/AutoFocusTextBox.cs
@@ -0,0 +1,13 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace BeachBody_Workout_Tracker
+{
+    public class AutoFocusTextBox : TextBox
+    {
+        public AutoFocusTextBox()
+        {
+            Loaded += delegate { Focus(FocusState.Programmatic); };
+        }
+    }
+}
